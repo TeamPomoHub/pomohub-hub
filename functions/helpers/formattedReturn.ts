adb6c74bb64a4a4fdf04cfe98d7f21d21bf376ab
MaxPrehoda <maxprehoda@gmail.com>
2022-10-26 10:14:58 -0700

    formatted returns was not properly named

--- functions/helpers/formattedReturn.ts
+++ functions/helpers/formattedReturn.ts
@@ -1,6 +1,8 @@
-module.exports = (statusCode, body) => {
+let formattedReturn = (statusCode : any, body : any) => {
     return {
         statusCode,
         body: JSON.stringify(body),
     };
 };
+
+module.exports = formattedReturn;
